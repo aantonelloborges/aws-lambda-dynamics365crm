a4c03be69c3f307065769967b86eae21abb11213
Angelo Antonello Borges <aantonelloborges@gmail.com>
2021-04-16 00:42:34 -0300

    Ajustes finais relativos ao AWS Connect

--- src/main/java/com/accenture/MSDynamics/HandlerGetAccount.java
+++ src/main/java/com/accenture/MSDynamics/HandlerGetAccount.java
@@ -7,53 +7,45 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 import com.accenture.msdynamics.entities.AuthorityEntity;
-import com.accenture.msdynamics.entities.GetAccountRequestEntity;
-import com.accenture.msdynamics.entities.GetAccountResponseEntity;
+import com.accenture.msdynamics.entities.Details;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class HandlerGetAccount implements RequestHandler<Object, String> {
+public class HandlerGetAccount implements RequestHandler<HashMap<String, Object>, HashMap<String, String>> {
   Gson gson = new GsonBuilder().setPrettyPrinting().create();
   LambdaLogger logger = null;
-  String response = null;
 
   @Override
-  public String handleRequest(Object event, Context context) {
+  public HashMap<String, String> handleRequest(HashMap<String, Object> event, Context context) {
     logger = context.getLogger();
-
-    /// logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
-    // logger.log("CONTEXT: " + gson.toJson(context));
-    // logger.log("EVENT: " + gson.toJson(event));
-    logger.log("EVENT: " + event);
-    // logger.log("EVENT TYPE: " + event.getClass());
-
-    String DYNAMICS_ORG_URI = System.getenv().get("DYNAMICS_ORG_URI");
-    String AZURE_TENANT_ID = System.getenv().get("AZURE_TENANT_ID");
-    String CLIENT_ID = System.getenv().get("CLIENT_ID");
-    String CLIENT_SECRET = System.getenv().get("CLIENT_SECRET");
-
-    Integer inicio = event.toString().indexOf("{phone=");
-    Integer fim = event.toString().length() - 25;
-    String event_json = event.toString().substring(inicio, fim);
-    logger.log("event_json: " + event_json);
-    GetAccountRequestEntity request = gson.fromJson(event_json, GetAccountRequestEntity.class);
-    logger.log("request.phone: " + request.phone);
+    HashMap<String, String> response = new HashMap<String, String>();
 
     try {
+      // logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+      String DYNAMICS_ORG_URI = System.getenv().get("DYNAMICS_ORG_URI");
+      String AZURE_TENANT_ID = System.getenv().get("AZURE_TENANT_ID");
+      String CLIENT_ID = System.getenv().get("CLIENT_ID");
+      String CLIENT_SECRET = System.getenv().get("CLIENT_SECRET");
+
+      logger.log("CONTEXT: " + gson.toJson(context));
+
+      logger.log("EVENT: " + event);
+      logger.log("event.get(\"Details\").toString(): " + event.get("Details").toString());
+      Details details = gson.fromJson(event.get("Details").toString().replace(":", "").replace("/", ""), Details.class);
+      logger.log("details.Parameters.phone: " + details.Parameters.phone);
 
       AuthorityEntity accessToken = authenticate(DYNAMICS_ORG_URI, AZURE_TENANT_ID, CLIENT_ID, CLIENT_SECRET, context);
 
-      response = getAccountData(DYNAMICS_ORG_URI, accessToken.access_token, request.phone, context).replace(":", "=").replace("\"", "");
-      //String response = gson.toJson(account_data);
-      logger.log("response: " + response);
+      return getAccountData(DYNAMICS_ORG_URI, accessToken.access_token, details.Parameters.phone, context);
 
     } catch (Exception ex) {
-      ex.printStackTrace();
+      response.put("ERRO", ex.getLocalizedMessage());
     }
 
     return response;
@@ -98,53 +90,56 @@ public class HandlerGetAccount implements RequestHandler<Object, String> {
     }
   }
 
-  public String getAccountData(String OrganizationURI, String accessToken, String phone_number,
+  public HashMap<String, String> getAccountData(String OrganizationURI, String accessToken, String phone_number,
       Context context) throws Exception {
 
     logger = context.getLogger();
 
     HttpURLConnection connection = null;
-    GetAccountResponseEntity contactEntity = null;
-    String prepeared_json = null;
+    HashMap<String, String> response = new HashMap<String, String>();
+
     try {
 
       URL url = new URL(OrganizationURI + "api/data/v9.2/contacts?$select=contactid,fullname,telephone1&$filter="
           + URLEncoder.encode("telephone1 eq '" + phone_number + "'", StandardCharsets.UTF_8.name()));
 
       connection = (HttpURLConnection) url.openConnection();
-      connection.addRequestProperty("Content-Type", "application/json");
-      connection.setRequestProperty("Accept", "*/*");
+      connection.addRequestProperty("Content-Type", "application/json; charset=utf-8");
+      connection.setRequestProperty("Accept", "application/json");
       connection.setRequestProperty("Connection", "keep-alive");
       connection.addRequestProperty("Authorization", "Bearer " + accessToken);
-
       connection.setRequestMethod("GET");
       connection.setDoOutput(true);
 
-      StringBuilder response = new StringBuilder();
+      StringBuilder response_dynamics = new StringBuilder();
       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
       String line = null;
       while ((line = bufferedReader.readLine()) != null) {
-        response.append(line);
+        response_dynamics.append(line);
       }
       bufferedReader.close();
+      logger.log("response_dynamics: " + response_dynamics.toString());
 
-      //String prepeared_json = response.toString().replace("\"@odata.context\":\"https://angeloborgesdynamics0.crm2.dynamics.com/api/data/v9.2/$metadata#contacts(contactid,fullname,telephone1)\",","").replace("\"value\":[{\"@odata.etag\":\"W/\\\"4750197\\\"\",", "\"data\":{").replace("}]}", "}}");
-      Integer inicio = response.toString().indexOf("\"contactid\"");
-      Integer fim = response.toString().length() - 2;
-      prepeared_json = response.toString().substring(inicio, fim);
-      prepeared_json = "{\"data\":{" + prepeared_json + "}";
-      logger.log("getAccountData.prepeared_json: " + prepeared_json);
-      //contactEntity = gson.fromJson(gson.toJson(prepeared_json), GetAccountResponseEntity.class);
-      //logger.log("contactEntity.data.fullname: " + contactEntity.data.fullname);
+      //TODO: encontrar uma forma melhor de transformar o oData do MSDynamics em um objeto/classe
+      String response_dynamics_lines[] = response_dynamics.toString().split(",");
+      for (String s : response_dynamics_lines) {
+        if (s.indexOf("contactid") != -1) {
+          response.put("Id", s.replaceAll("\"", "").replace("fullname:", ""));
+        } else if (s.indexOf("fullname") != -1) {
+          response.put("Nome", s.replaceAll("\"", "").replace("fullname:", ""));
+        } else if (s.indexOf("telephone1") != -1) {
+          response.put("Telefone", s.replaceAll("\"", "").replace("fullname:", ""));
+        }
+      }
 
     } catch (Exception ex) {
-      ex.printStackTrace();
+      response.put("ERRO", ex.getLocalizedMessage());
     } finally {
       if (connection != null) {
         connection.disconnect();
       }
     }
-    return prepeared_json;
-  }
 
+    return response;
+  }
 }
